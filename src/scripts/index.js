ed365723d7c26eaf1a323d067e8fbb61e562f3bc
CarlosFreitas <carlos.d.freitas0@hotmail.com>
2023-04-19 19:54:25 -0300

    Layout,DarkTheme,RenderAlbum,RenderBTNGender - Finish

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,94 @@
 /* Desenvolva sua lógica aqui ... */
+const renderAlbum = list =>{
+    const albumList = document.querySelector('.album__list')
+    albumList.innerHTML = ''
+
+    list.forEach(item => {
+        albumList.appendChild(createAlbumItem(item))
+    });
+}
+const createAlbumItem = item =>{
+    const li = document.createElement('li')
+    const figure = document.createElement('figure')
+    const img = document.createElement('img')
+    const div = document.createElement('div')
+    const p = document.createElement('p')
+    const span = document.createElement('span')
+    const h2 = document.createElement('h2')
+    const div2 = document.createElement('div')
+    const p2 = document.createElement('p')
+
+    li.classList = 'album__item'
+    p.classList = 'album__artist-band font3'
+    span.classList = 'album__release font3'
+    h2.classList = 'album__name font2'
+    p2.classList = 'album__value font3'
+
+    img.src = item.img
+    p.innerText = item.band
+    span.innerText = item.year
+    h2.innerText = item.title
+    p2.innerText = `R$ ${item.price.toFixed(2)}`
+
+    li.append(figure, div)
+    figure.appendChild(img)
+    div.append(p, h2, div2)
+    p.appendChild(span)
+    div2.append(p2, btnBuy(item))
+    
+    return li
+}
+const btnBuy = item =>{
+    const buttonBuy = document.createElement('button')
+    buttonBuy.innerText = 'Comprar'
+    buttonBuy.id = item.id
+    // buttonBuy.addEventListener('click', (e)=> {
+    //     console.log(e.target)
+    // })
+    return buttonBuy
+}
+// -----------------------------------------------------------------------
+const renderGenderButtons = genderList => {
+    const listGenderBtn = document.querySelector('.gender__buttons')
+
+    genderList.forEach(genderBtn => {
+        listGenderBtn.appendChild(createGenderButtons(genderBtn))
+    })
+}
+
+const createGenderButtons = gender => {
+    const ListGender = categories
+    const li = document.createElement('li')
+    const label = document.createElement('label')
+    const radio = document.createElement('input')
+    li.classList = 'gender-btn__item'
+
+    label.innerText = gender
+    radio.type = 'radio'
+    radio.name = 'teste'
+    radio.type = 'hidden'
+    console.log()
+
+    const findIndexGender = ListGender.findIndex(gender => gender === label.innerText)
+    label.htmlFor = findIndexGender
+    radio.id = findIndexGender
+    
+    // li.addEventListener('click', ()=> {
+    //     ListGender.forEach(item =>{
+    //         if(label.innerText == item){
+    //             li.classList.add('clicked')
+    //             console.log(item ,gender)
+    //         }else if(label.innerText != item){
+    //             li.classList.remove('clicked')
+    //         }
+    //     })
+    // })
+
+
+    li.append(label, radio)
+
+    return li
+}
+
+renderAlbum(products)
+renderGenderButtons(categories)
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,32 @@
 /* Desenvolva sua lógica aqui ... */
+const themeChange = () => {
+    const themeBtn = document.querySelector('.header__theme')
+    const html = document.querySelector('html')
+    const img = document.createElement('img')
+    img.src = '/open-music-base_CarloslFreitas/src/assets/img/moon.svg'
+    themeBtn.appendChild(img)
+
+    const themeSave = JSON.parse(localStorage.getItem('dark__mode'))
+
+    if(themeSave){
+        img.src = '/open-music-base_CarloslFreitas/src/assets/img/sun.svg'
+        html.classList.add('dark__mode')
+    }else{
+        img.src = '/open-music-base_CarloslFreitas/src/assets/img/moon.svg'
+        html.classList.remove('dark__mode')
+    }
+
+    themeBtn.addEventListener('click', ()=>{
+        html.classList.toggle('dark__mode')
+
+        if(html.classList.contains('dark__mode')){
+            img.src = '/open-music-base_CarloslFreitas/src/assets/img/sun.svg'
+            localStorage.setItem('dark__mode', true)
+        }else{
+            img.src = '/open-music-base_CarloslFreitas/src/assets/img/moon.svg'
+            localStorage.setItem('dark__mode', false)
+        }
+    })
+}
+
+themeChange()
